3e8a9cc9cf1d186724342b1d45cc2b9f5320fbd9
lexuanduy <lexuanduy07@gmail.com>
2019-05-23 18:01:15 +0700

    create home page, list user, user detail

--- ListUser.php
+++ ListUser.php
@@ -1,67 +1,249 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
+
 <head>
-	<title>List User</title>
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-<style>
-	.avatar img {
-    border-radius: 5px;
-    width: 50px;
-    margin-right: 10px;
-}
-img {
-    max-width: 100%;
-    height: auto;
-}
-.name {
-    font-size: 15px;
-    font-weight: 500;
-    color: #333;
-}
-</style>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>List user</title>
+    <meta name="description" content="Free Bootstrap Theme by BootstrapMade.com">
+    <meta name="keywords" content="free website templates, free bootstrap themes, free template, free bootstrap, free website template">
+
+    <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Fira+Sans|Roboto:300,400|Questrial|Satisfy">
+    <link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/animate.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+
+    <!-- =======================================================
+    Theme Name: Laura
+    Theme URL: https://bootstrapmade.com/laura-free-creative-bootstrap-theme/
+    Author: BootstrapMade.com
+    Author URL: https://bootstrapmade.com
+  ======================================================= -->
+
+    <style>
+        .avatar img {
+            border-radius: 5px;
+            width: 50px;
+            margin-right: 10px;
+        }
+
+        img {
+            max-width: 100%;
+            height: auto;
+        }
+
+        .name {
+            font-size: 15px;
+            font-weight: 500;
+            color: #333;
+        }
+        #myGrid {
+            text-align:center;
+            
+        }
+    </style>
 </head>
-<body>
 
-<?php
-	$servername = "localhost:3306";
-	$database = "facebook";
-	$username = "root";
-	$password = "";
+<body id="myPage" data-spy="scroll" data-target=".navbar" data-offset="60">
+    <div class="header">
+        <div class="bg-color">
+            <header id="main-header">
+                <nav class="navbar navbar-default navbar-fixed-top">
+                    <div class="container">
+                        <div class="navbar-header">
+                            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#lauraMenu">
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                            </button>
+                            <a class="navbar-brand" href="#">Laura</a>
+                        </div>
+                        <div class="collapse navbar-collapse" id="lauraMenu">
+                            <ul class="nav navbar-nav navbar-right navbar-border">
+                                <li class="active"><a href="/Userinformation/home.php">Home</a></li>
+                                <li><a href="#about">About</a></li>
+                                <li><a href="/Userinformation/ListUser.php">Users</a></li>
+                                <!-- <li><a href="#testimonial">Testimonial</a></li> -->
+                                <li><a href="#contact">Contact Us</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+            </header>
+            <div class="wrapper">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-12 wow fadeIn delay-05s">
+                            <div class="banner-text">
+                                <h2>Hi, I am <span>Laura</span> Thomson,</h2>
+                                <p>A Creative Photographer <br>& Designer</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <?php
+    $servername = "localhost:3306";
+    $database = "facebook";
+    $username = "root";
+    $password = "";
 
 
-	$conn = mysqli_connect($servername, $username, $password, $database);
-	if (!$conn) {
-	    die("Connection failed: " . mysqli_connect_error());
-	}
+    $conn = mysqli_connect($servername, $username, $password, $database);
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
 
-	$result = mysqli_query($conn,"SELECT uid, phone FROM experiment limit 20");
-	$option = '';
-	while($row = mysqli_fetch_array($result))
-	{
-		
-		$linkAvatar = 'https://graph.facebook.com/'.$row['uid'].'/picture';
-		$linkFace = 'https://www.facebook.com/'.$row['uid'];
-		$linkDetail = '/UserInformation/Profile.php'.'?phone='.$row['phone'];
-		$option .= '
-			<div class="col-md-4">
-				<div class="avartar">
-					<img src="'.$linkAvatar.'" width="50" alt="">
-				</div>
-				<div class="info">
-					<span class="name">
-						<a href='.$linkDetail.'>'.$row['phone'].'</a>
-					</span>	
-				</div>						
-			</div>';
-}
+    function mysqli_select($conn, $query)
+    {
+        $rows = array();
+        $result = mysqli_query($conn, $query);
 
-	mysqli_close($conn);
-?>
-<div class="row">
-<?php echo $option; ?>
-</div>
+        // If query failed, return `false`
+        if ($result === false) {
+            return false;
+        }
+
+        // If query was successful, retrieve all the rows into an array
+        while ($row = mysqli_fetch_array($result)) {
+            $rows[] = $row;
+        }
+        return $rows;
+    }
+    $rows = mysqli_select($conn, "SELECT uid, phone FROM experiment limit 20");
+    if ($rows === false) {
+        $error = db_error();
+    }
+
+    mysqli_close($conn);
+    ?>
+    <section id="portfolio" class="section-padding wow fadeInUp delay-05s">
+        <div class="container">
+            <div class="row">
+                
+                <div class="col-md-12">
+                    <h2 class="title text-center">List <span class="deco">User</span></h2>
+                </div>
+                <div class="col-md-12">
+                    <div id="myGrid" class="grid-padding">
+                        <?php foreach ($rows as $value) : ?>
+                            <div class="col-md-3 col-sm-3 padding-right-zero">
+                                <div class="avartar">
+                                        <img src="https://graph.facebook.com/<?php echo $value['uid']; ?>/picture" width="100" alt="">
+                                </div>
+                                <div class="info">
+                                        <span class="name">
+                                            <a href=/UserInformation/profile.php?phone=<?php echo $value['phone']; ?>><?php echo $value['phone']; ?></a>
+                                        </span>
+                                </div>
+                            </div>
+                        <?php endforeach; ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section id="testimonial" class="section-padding wow fadeInUp">
+    <div class="container">
+      <div class="row">
+        <h2 class="title text-center">See What Our <span class="deco">Client</span> Are Saying ?</h2>
+        <div class="test-sec">
+          <div class="col-sm-4">
+            <blockquote>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sed dapibus leo nec ornare diamsed commodo nibh ante facilisis bibendum dolor feugiat at. </p>
+            </blockquote>
+            <div class="carousel-info">
+              <div class="pull-left"> <span class="testimonials-name">John Doe</span> <span class="testimonials-post">CEO, Company Inc.</span> </div>
+            </div>
+          </div>
+          <div class="col-sm-4">
+            <blockquote>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sed dapibus leo nec ornare diamsed commodo nibh ante facilisis bibendum dolor feugiat at. </p>
+            </blockquote>
+            <div class="carousel-info">
+              <div class="pull-left"> <span class="testimonials-name">John Doe</span> <span class="testimonials-post">CEO, Company Inc.</span> </div>
+            </div>
+          </div>
+          <div class="col-sm-4">
+            <blockquote>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sed dapibus leo nec ornare diamsed commodo nibh ante facilisis bibendum dolor feugiat at. </p>
+            </blockquote>
+            <div class="carousel-info">
+              <div class="pull-left"> <span class="testimonials-name">John Doe</span> <span class="testimonials-post">CEO, Company Inc.</span> </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section id="contact" class="section-padding wow fadeIn delay-05s">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="contact-sec text-center">
+            <h2>Want To <span class="deco">Hire</span> Me?</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt.</p>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </section>
+
+  <footer class="footer-2 text-center-xs bg--white">
+    <div class="container">
+      <!--end row-->
+      <div class="row">
+        <div class="col-md-6">
+          <div class="footer">
+            © Copyright Laura Theme. All Rights Reserved
+            <div class="credits">
+              <!--
+                All the links in the footer should remain intact. 
+                You can delete the links only if you purchased the pro version.
+                Licensing information: https://bootstrapmade.com/license/
+                Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/buy/?theme=Laura
+              -->
+              Designed by <a href="https://bootstrapmade.com/">BootstrapMade.com</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-6 text-right">
+          <ul class="social-list">
+            <li>
+              <a href="#"><i class="fa fa-twitter"></i></a>
+            </li>
+            <li>
+              <a href="#"><i class="fa fa-dribbble"></i></a>
+            </li>
+            <li>
+              <a href="#"><i class="fa fa-vimeo"></i></a>
+            </li>
+            <li>
+              <a href="#"><i class="fa fa-instagram"></i></a>
+            </li>
+          </ul>
+        </div>
+
+      </div>
+      <!--end row-->
+    </div>
+  </footer>
+
+<script src="js/jquery.min.js"></script>
+  <script src="js/jquery.easing.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+
+  <script src="js/wow.js"></script>
+
+  <script src="contactform/contactform.js"></script>
+
+
 
 </body>
+
 </html>
--- Profile.php
+++ Profile.php
@@ -1,55 +1,111 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
+
 <head>
-	<title>Profile User</title>
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>User Profile</title>
+	<meta name="description" content="Free Bootstrap Theme by BootstrapMade.com">
+	<meta name="keywords" content="free website templates, free bootstrap themes, free template, free bootstrap, free website template">
+
+	<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Fira+Sans|Roboto:300,400|Questrial|Satisfy">
+	<link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/animate.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
 	<style type="text/css">
 		.card {
-  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
-  max-width: 300px;
-  margin: auto;
-  text-align: center;
-  font-family: arial;
-}
+			box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+			max-width: 300px;
+			margin: auto;
+			text-align: center;
+			font-family: arial;
+		}
 
-.title {
-  color: grey;
-  font-size: 18px;
-}
+		.title {
+			color: grey;
+			font-size: 18px;
+		}
 
-button {
-  border: none;
-  outline: 0;
-  display: inline-block;
-  padding: 8px;
-  color: white;
-  background-color: #000;
-  text-align: center;
-  cursor: pointer;
-  width: 100%;
-  font-size: 18px;
-}
+		button {
+			border: none;
+			outline: 0;
+			display: inline-block;
+			padding: 8px;
+			color: white;
+			background-color: #000;
+			text-align: center;
+			cursor: pointer;
+			width: 100%;
+			font-size: 18px;
+		}
 
-a {
-  text-decoration: none;
-  font-size: 22px;
-  color: black;
-}
+		a {
+			text-decoration: none;
+			font-size: 22px;
+			color: black;
+		}
 
-button:hover, a:hover {
-  opacity: 0.7;
-}
+		button:hover,
+		a:hover {
+			opacity: 0.7;
+		}
+
+		#myGrid {
+			text-align: center;
+		}
 	</style>
 </head>
-<body>
 
-<?php
+<body id="myPage" data-spy="scroll" data-target=".navbar" data-offset="60">
+	<div class="header">
+		<div class="bg-color">
+			<header id="main-header">
+				<nav class="navbar navbar-default navbar-fixed-top">
+					<div class="container">
+						<div class="navbar-header">
+							<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#lauraMenu">
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+							</button>
+							<a class="navbar-brand" href="#">Laura</a>
+						</div>
+						<div class="collapse navbar-collapse" id="lauraMenu">
+							<ul class="nav navbar-nav navbar-right navbar-border">
+								<li class="active"><a href="/Userinformation/home.php">Home</a></li>
+								<li><a href="#about">About</a></li>
+								<li><a href="/Userinformation/ListUser.php">Users</a></li>
+								<!-- <li><a href="#testimonial">Testimonial</a></li> -->
+								<li><a href="#contact">Contact Us</a></li>
+							</ul>
+						</div>
+					</div>
+				</nav>
+			</header>
+			<div class="wrapper">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-12 wow fadeIn delay-05s">
+							<div class="banner-text">
+								<h2>Hi, I am <span>Laura</span> Thomson,</h2>
+								<p>A Creative Photographer <br>& Designer</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<?php
 	$servername = "localhost:3306";
 	$database = "facebook";
 	$username = "root";
 	$password = "";
 	$conn = mysqli_connect($servername, $username, $password, $database);
 	if (!$conn) {
-	    die("Connection failed: " . mysqli_connect_error());
+		die("Connection failed: " . mysqli_connect_error());
 	}
 	$phone = $_GET['phone'];
 
@@ -57,23 +113,23 @@ button:hover, a:hover {
 	$result = $conn->query($sql);
 	$uid;
 	$phone;
-	
+
 	if ($result->num_rows > 0) {
-	    while($row = $result->fetch_assoc()) {
-	        
-	        $uid = $row["uid"];
-	        $phone = $row["phone"];
-	    }
+		while ($row = $result->fetch_assoc()) {
+
+			$uid = $row["uid"];
+			$phone = $row["phone"];
+		}
 	} else {
-	    echo "0 results";
+		echo "0 results";
 	}
-	$linkAvatar = 'https://graph.facebook.com/'.$uid.'/picture';
-	$linkFace = 'https://www.facebook.com/'.$uid;
+	$linkAvatar = 'https://graph.facebook.com/' . $uid . '/picture';
+	$linkFace = 'https://www.facebook.com/' . $uid;
 
 	$conn->close();
-?>
+	?>
 
-<h2 style="text-align:center">User Profile Card</h2>
+	<!-- <h2 style="text-align:center">User Profile Card</h2>
 
 <div class="card">
 	<img src=<?php echo $linkAvatar ?> alt="" style="width:50%">
@@ -85,7 +141,85 @@ button:hover, a:hover {
     <a href=<?php echo $linkFace ?> id="linkFace"><i class="fa fa-facebook"></i></a> 
   </div>
   
-</div>
+</div> -->
+	<section id="portfolio" class="section-padding wow fadeInUp delay-05s">
+		<div class="container">
+			<div class="row">
+				<div class="card">
+					<img src=<?php echo $linkAvatar ?> alt="" style="width:50%">
+					<h1 class="name" id="name"><?php echo $uid ?></h1>
+					<p class="title" id="education"><?php echo $phone ?></p>
+					<p class="university" id="university">Harvard University</p>
+					<div style="margin: 24px 0;">
+
+						<a href=<?php echo $linkFace ?> id="linkFace"><i class="fa fa-facebook"></i></a>
+					</div>
+
+				</div>
+			</div>
+		</div>
+	</section>
+	<section id="contact" class="section-padding wow fadeIn delay-05s">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<div class="contact-sec text-center">
+						<h2>Want To <span class="deco">Hire</span> Me?</h2>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt.</p>
+					</div>
+				</div>
+
+			</div>
+		</div>
+	</section>
+
+	<footer class="footer-2 text-center-xs bg--white">
+		<div class="container">
+			<!--end row-->
+			<div class="row">
+				<div class="col-md-6">
+					<div class="footer">
+						© Copyright Laura Theme. All Rights Reserved
+						<div class="credits">
+							<!--
+                All the links in the footer should remain intact. 
+                You can delete the links only if you purchased the pro version.
+                Licensing information: https://bootstrapmade.com/license/
+                Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/buy/?theme=Laura
+              -->
+							Designed by <a href="https://bootstrapmade.com/">BootstrapMade.com</a>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-6 text-right">
+					<ul class="social-list">
+						<li>
+							<a href="#"><i class="fa fa-twitter"></i></a>
+						</li>
+						<li>
+							<a href="#"><i class="fa fa-dribbble"></i></a>
+						</li>
+						<li>
+							<a href="#"><i class="fa fa-vimeo"></i></a>
+						</li>
+						<li>
+							<a href="#"><i class="fa fa-instagram"></i></a>
+						</li>
+					</ul>
+				</div>
+
+			</div>
+			<!--end row-->
+		</div>
+	</footer>
+	<script src="js/jquery.min.js"></script>
+	<script src="js/jquery.easing.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+
+	<script src="js/wow.js"></script>
+
+	<script src="contactform/contactform.js"></script>
 
 </body>
+
 </html>
